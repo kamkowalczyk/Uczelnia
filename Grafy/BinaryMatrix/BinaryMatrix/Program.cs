7359497b15ee40b659fbc5b74b96eb82a748eb1f
kamkowalczyk <79981280+kamkowalczyk@users.noreply.github.com>
2024-07-17 11:14:32 +0200

    VRP

---
+++ Grafy/BinaryMatrix/BinaryMatrix/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BinaryMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int m = dimensions[0];
+            int n = dimensions[1];
+   
+            var rowSums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+          
+            var colSums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            long rowSumTotal = rowSums.Sum();
+            long colSumTotal = colSums.Sum();
+
+            if (rowSumTotal == colSumTotal)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+        }
+    }
+}
